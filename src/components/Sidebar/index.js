e7532f0ac70cf19d940f99a1a4b6038499ecbe21
Eric Ong <ericong18@gmail.com>
2021-11-04 23:41:18 -0700

    Add dingo to sidebar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import dingoLogo from '../../assets/dingo.svg';
 import styles from './styles.module.css';
 
 const Sidebar = () => {
-    return <nav className={styles.nav}>Sidebar</nav>;
+    return (
+        <nav className={styles.nav}>
+            <img className={styles.logo} src={dingoLogo} />
+        </nav>
+    );
 };
 
 export default Sidebar;
