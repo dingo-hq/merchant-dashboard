346c1141539e53813f73b423714a97e9b87c2836
Eric Ong <ericong18@gmail.com>
2021-11-04 23:55:52 -0700

    Add buttons to sidebar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,11 +1,48 @@
 import React from 'react';
+import {
+    IconButton,
+    CogIcon,
+    LogOutIcon,
+    GridViewIcon,
+    Tooltip,
+    Position,
+} from 'evergreen-ui';
 import dingoLogo from '../../assets/dingo.svg';
 import styles from './styles.module.css';
 
 const Sidebar = () => {
     return (
         <nav className={styles.nav}>
-            <img className={styles.logo} src={dingoLogo} />
+            <div className={styles.mainActions}>
+                <img className={styles.logo} src={dingoLogo} />
+                <ul className={styles.navItems}>
+                    <li>
+                        <Tooltip content="Dashboard" position={Position.RIGHT}>
+                            <IconButton
+                                icon={GridViewIcon}
+                                appearance="minimal"
+                                size="large"
+                            />
+                        </Tooltip>
+                    </li>
+                    <li>
+                        <Tooltip content="Settings" position={Position.RIGHT}>
+                            <IconButton
+                                icon={CogIcon}
+                                appearance="minimal"
+                                size="large"
+                            />
+                        </Tooltip>
+                    </li>
+                </ul>
+            </div>
+            <Tooltip content="Log out" position={Position.RIGHT}>
+                <IconButton
+                    icon={LogOutIcon}
+                    appearance="minimal"
+                    size="large"
+                />
+            </Tooltip>
         </nav>
     );
 };
