aff4943ef0cd80064fb62ae51bcbe81275d467c6
Eric Ong <ericong18@gmail.com>
2021-11-06 14:28:25 -0700

    Set active background for nav item, add box shadow to navbar

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,43 +1,50 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import { CogIcon, LogOutIcon, GridViewIcon, Button } from 'evergreen-ui';
+import classNames from 'classnames';
 import logo from '../../assets/logo.png';
 import styles from './styles.module.css';
 
 const Navigation = () => {
     const navigate = useNavigate();
+    const location = useLocation();
+    const { pathname } = location;
+
+    const navItems = [
+        {
+            icon: GridViewIcon,
+            path: '/dashboard/stats',
+            label: 'Statistics',
+        },
+        {
+            icon: CogIcon,
+            path: '/dashboard/settings',
+            label: 'Settings',
+        },
+    ];
 
     return (
         <nav className={styles.nav}>
             <div className={styles.mainActions}>
                 <img className={styles.logo} src={logo} />
                 <ul className={styles.navItems}>
-                    <li>
+                    {navItems.map(({ icon, path, label }) => (
                         <Button
-                            iconBefore={GridViewIcon}
-                            appearance="minimal"
                             size="large"
-                            onClick={() => {
-                                navigate('/dashboard/stats');
-                            }}
-                            className={styles.item}
-                        >
-                            Statistics
-                        </Button>
-                    </li>
-                    <li>
-                        <Button
-                            iconBefore={CogIcon}
                             appearance="minimal"
-                            size="large"
+                            className={classNames(
+                                styles.item,
+                                pathname === path && styles.active,
+                            )}
+                            key={path}
+                            iconBefore={icon}
                             onClick={() => {
-                                navigate('/dashboard/settings');
+                                navigate(path);
                             }}
-                            className={styles.item}
                         >
-                            Settings
+                            {label}
                         </Button>
-                    </li>
+                    ))}
                 </ul>
             </div>
             <Button
