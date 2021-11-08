48228338525911dc3e3f08857ff9d2e5d4dd2a28
Eric Ong <ericong18@gmail.com>
2021-11-07 23:05:43 -0800

    Refactor status indicator and add error

--- src/pages/Dashboard/Statistics/index.js
+++ src/pages/Dashboard/Statistics/index.js
@@ -7,13 +7,27 @@ import DashboardPage from '../../../components/DashboardPage';
 import styles from './styles.module.css';
 
 const Statistics = (props) => {
+    const statusContent = {
+        success: {
+            color: 'success',
+            text: 'Updated every 30 seconds',
+        },
+        error: {
+            color: 'danger',
+            text: 'Failed to update at this time',
+        },
+    };
+
     return (
         <DashboardPage
             heading="Statistics"
             subheading="Take a peek at some numbers and see how you're doing at a quick glance"
             sideElement={
-                <StatusIndicator color="success" className={styles.status}>
-                    Data is updated every 30 seconds
+                <StatusIndicator
+                    color={statusContent.success.color}
+                    className={styles.status}
+                >
+                    {statusContent.success.text}
                 </StatusIndicator>
             }
         >
