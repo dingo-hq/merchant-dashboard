66f56b9e30e151aed27981e8da97116a4e7c9783
Eric Ong <ericong18@gmail.com>
2021-11-18 21:13:15 -0800

    Add options for discount method

--- src/pages/Dashboard/Settings/index.js
+++ src/pages/Dashboard/Settings/index.js
@@ -1,39 +1,62 @@
 import { Button, TextInputField, toaster } from 'evergreen-ui';
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import getMerchantDetails from '../../../api/getMerchantDetails';
 import saveSettings from '../../../api/saveSettings';
 import DashboardPage from '../../../components/DashboardPage';
 import Toggle from '../../../components/Toggle';
+import priceTag from '../../../assets/price-tag-circle.svg';
+import shoppingsBags from '../../../assets/shopping-bags-circle.svg';
 import styles from './styles.module.css';
 
 const PAUSED = 'paused';
-const PROMOTIONAL_DISCOUNT_ENABLED = 'promotionalDiscountEnabled';
+const DISCOUNT_ENABLED = 'discountEnabled';
 const PROMOTIONAL_DISCOUNT_NUMBER = 'promotionalDiscountNumber';
 
 const toggles = [
     {
-        name: PROMOTIONAL_DISCOUNT_ENABLED,
-        label: 'Enable promotional discounts',
-        note: 'Control whether or not you want customers to receive promotional codes after submitting a recommended item.',
+        name: DISCOUNT_ENABLED,
+        label: 'Enable discounts',
+        note: 'Control whether or not you want customers to receive promotional discounts after submitting a recommended item',
     },
     {
         name: PAUSED,
         label: 'Pause Dingo',
-        note: 'Turning this setting ON means customers will stop receiving recommendation links following a purchase until this setting is turned OFF.',
+        note: 'Turning this setting ON means customers will stop receiving recommendation links following a purchase until this setting is turned OFF',
     },
 ];
 
 const initialSettings = {
     [PAUSED]: false,
-    [PROMOTIONAL_DISCOUNT_ENABLED]: false,
+    [DISCOUNT_ENABLED]: false,
     [PROMOTIONAL_DISCOUNT_NUMBER]: 0,
 };
+
+const promotionTypes = {
+    PERCENTAGE_DISCOUNT: 'PERCENTAGE_DISCOUNT',
+    LOYALTY_POINTS: 'LOYALTY_POINTS',
+};
+
+const promotionMethods = [
+    {
+        label: 'Percentage Discount',
+        imageSrc: priceTag,
+        type: promotionTypes.PERCENTAGE_DISCOUNT,
+    },
+    {
+        label: 'Loyalty Points',
+        imageSrc: shoppingsBags,
+        type: promotionTypes.LOYALTY_POINTS,
+    },
+];
 
 const Settings = ({ pageName }) => {
     const [settings, setSettings] = useState(initialSettings);
     const [isLoading, setIsLoading] = useState(false);
     const [isSaving, setIsSaving] = useState(false);
+    const [selectedPromotionMethod, setSelectedPromotionMethod] =
+        useState(null);
 
     useEffect(() => {
         const fetchMerchantDetails = async () => {
@@ -74,6 +97,47 @@ const Settings = ({ pageName }) => {
         }
     };
 
+    const percentageDiscountInput = (
+        <TextInputField
+            label="Percentage discount amount"
+            description="The percentage discount that the customer can use when purchasing a recommended item"
+            type="number"
+            min={0}
+            max={100}
+            value={settings[PROMOTIONAL_DISCOUNT_NUMBER]}
+            disabled={isLoading || isSaving}
+            onChange={(e) =>
+                handleSettingChange(
+                    parseInt(e.target.value),
+                    PROMOTIONAL_DISCOUNT_NUMBER,
+                )
+            }
+        />
+    );
+
+    const loyaltyPointsInput = (
+        <TextInputField
+            label="Loyalty point amount"
+            description="The amount of loyalty points that the customer can redeem when purchasing a recommended item"
+            type="number"
+            min={0}
+            max={100}
+            value={settings[PROMOTIONAL_DISCOUNT_NUMBER]}
+            disabled={isLoading || isSaving}
+            onChange={(e) =>
+                handleSettingChange(
+                    parseInt(e.target.value),
+                    PROMOTIONAL_DISCOUNT_NUMBER,
+                )
+            }
+        />
+    );
+
+    const promotionInput =
+        selectedPromotionMethod === promotionTypes.PERCENTAGE_DISCOUNT
+            ? percentageDiscountInput
+            : loyaltyPointsInput;
+
     return (
         <DashboardPage
             heading={pageName}
@@ -83,12 +147,46 @@ const Settings = ({ pageName }) => {
             <section>
                 <ul className={styles.settings}>
                     <li>
+                        <span className={styles.promotionOptionsLabel}>
+                            Discount method
+                        </span>
+                        <span className={styles.promotionOptionsDescription}>
+                            When a customer selects a recommended item through a
+                            recommendation link, they may receive a percentage
+                            discount or loyalty points upon redemption
+                        </span>
+                        <div className={styles.promotionOptions}>
+                            {promotionMethods.map(
+                                ({ label, imageSrc, type }) => (
+                                    <figure
+                                        key={label}
+                                        className={classNames(
+                                            styles.promotionOption,
+                                            selectedPromotionMethod === type &&
+                                                styles.promotionOptionSelected,
+                                        )}
+                                        onClick={() =>
+                                            setSelectedPromotionMethod(type)
+                                        }
+                                    >
+                                        <img
+                                            src={imageSrc}
+                                            className={
+                                                styles.promotionOptionImage
+                                            }
+                                        />
+                                        <figcaption>{label}</figcaption>
+                                    </figure>
+                                ),
+                            )}
+                        </div>
+                    </li>
+                    <li>{promotionInput}</li>
+                    {/* <li>
                         <TextInputField
-                            label="Promotional discount (%)"
-                            description="The promotion code value that appears after a user selects and submits a recommended item from a recommendation link"
-                            type="number"
-                            min={0}
-                            max={100}
+                            label="Discount duration"
+                            description="The amount of time the discount is valid for"
+                            type="date"
                             value={settings[PROMOTIONAL_DISCOUNT_NUMBER]}
                             disabled={isLoading || isSaving}
                             onChange={(e) =>
@@ -98,7 +196,7 @@ const Settings = ({ pageName }) => {
                                 )
                             }
                         />
-                    </li>
+                    </li> */}
                     {toggles.map(({ name, label, note }) => (
                         <Toggle
                             key={name}
