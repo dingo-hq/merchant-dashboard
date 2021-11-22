6bd1add82155eb038e6a9fbe19f5aa2d36407a2b
Eric Ong <ericong18@gmail.com>
2021-11-21 23:12:08 -0800

    Fixes

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,11 +1,5 @@
 import config from '../config';
 
-console.log('config is', config);
-console.log(
-    'process.env.REACT_APP_ENVIRONMENT',
-    process.env.REACT_APP_ENVIRONMENT,
-);
-
 export const BASE_URL = config[process.env.REACT_APP_ENVIRONMENT].baseUrl;
 export const SQUARE_OAUTH_URL = `${BASE_URL}/oauth`;
 export const APP_LOGOUT_URL = `${BASE_URL}/oauth/logout`;
